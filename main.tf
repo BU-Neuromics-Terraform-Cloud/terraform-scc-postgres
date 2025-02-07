94f814c4c99930c5a6de8dcdad2349b8ee04fc6d
Adam Labadorf <alabadorf@gmail.com>
2025-02-07 12:14:46 -0500

    Add dynamic instance naming for PostgreSQL Singularity deployment
    - Generate unique instance ID using uuid()
    - Dynamically create instance name with unique identifier
    - Update provisioner commands to use dynamic instance name
    - Add triggers to resources to track instance name across lifecycle

--- main.tf
+++ main.tf
@@ -20,6 +20,8 @@ locals {
   postgres_db = var.postgres_db
   # Use environment variable or change this
   postgres_password = var.postgres_password
+  instance_id = uuid()
+  postgres_instance_name = "postgres_instance_${local.instance_id}"
 }
 
 # Pull PostgreSQL image
@@ -45,6 +47,11 @@ resource "null_resource" "setup_directories" {
 resource "null_resource" "postgres_instance" {
   depends_on = [null_resource.postgres_pull]
 
+  # Add triggers to store the instance name
+  triggers = {
+    instance_name = local.postgres_instance_name
+  }
+
   provisioner "local-exec" {
     command = <<-EOT
       singularity instance start \
@@ -52,7 +59,7 @@ resource "null_resource" "postgres_instance" {
         --bind ${local.postgres_data}/logs:/var/log/postgresql/ \
         --bind ${local.postgres_data}/run:/var/run/postgresql/ \
         --bind ${local.postgres_data}/tmp:/tmp/ \
-        postgres.sif postgres_instance 
+        postgres.sif ${local.postgres_instance_name}
     EOT
   }
 
@@ -60,8 +67,8 @@ resource "null_resource" "postgres_instance" {
     when    = destroy
     command = <<-EOT
       # Stop the postgres instance if it's running
-      if singularity instance list | grep -q postgres_instance; then
-        singularity instance stop postgres_instance
+      if singularity instance list | grep -q ${self.triggers.instance_name}; then
+        singularity instance stop ${self.triggers.instance_name}
       fi
     EOT
   }
@@ -73,6 +80,11 @@ resource "null_resource" "init_database" {
     null_resource.setup_directories,
     null_resource.postgres_instance,
   ]
+
+  # Add triggers to store the instance name
+  triggers = {
+    instance_name = local.postgres_instance_name
+  }
 
   provisioner "local-exec" {
     command = <<-EOT
@@ -99,7 +111,7 @@ resource "null_resource" "init_database" {
       chmod +x ${local.postgres_data}/tmp/postgres_start.sh
 
       # initialize the database
-      singularity exec instance://postgres_instance /tmp/postgres_start.sh
+      singularity exec instance://${self.triggers.instance_name} /tmp/postgres_start.sh
     EOT
   }
 } 
@@ -107,16 +119,20 @@ resource "null_resource" "init_database" {
 resource "null_resource" "postgres_start" {
   depends_on = [null_resource.init_database]
 
+  triggers = {
+    instance_name = local.postgres_instance_name
+  }
+
   provisioner "local-exec" {
-    command = "singularity exec instance://postgres_instance pg_ctl start -D /var/lib/postgresql/data/ -l /var/log/postgresql/logfile"
+    command = "singularity exec instance://${local.postgres_instance_name} pg_ctl start -D /var/lib/postgresql/data/ -l /var/log/postgresql/logfile"
   }
 
   provisioner "local-exec" {
     when = destroy
     command = <<-EOT
       # Stop the postgres instance if it's running
-      if singularity instance list | grep -q postgres_instance; then
-        singularity exec instance://postgres_instance pg_ctl stop -D /var/lib/postgresql/data/ -m fast
+      if singularity instance list | grep -q ${self.triggers.instance_name}; then
+        singularity exec instance://${self.triggers.instance_name} pg_ctl stop -D /var/lib/postgresql/data/ -m fast
       fi
     EOT
   }
